a923142386cfebb21c4f851afb1c3e9cd1bf906c
franciscodalbertas <francisco.albertas@gmail.com>
2022-08-26 16:27:47 -0300

    adicionando loop

--- scripts/exporting_urban.R
+++ scripts/exporting_urban.R
@@ -34,33 +34,36 @@ scen_to_keep <- c("TH_TFBASE_TCBASE_NOBIOD_NOTECH_NODEM_SPA0_SSP2",
 
 scen_subset <- grep(pattern =paste(scen_to_keep,collapse = "|"),x = scen,value = T )
 
+# como ja terminei o cen "TH_TF2000_TCBASE_BIOD_NOTECH_NODEM_SPA0_SSP2" exclui ele do loop
 
-# folder to save the results
+scen_to_keep <- scen_to_keep[-4]
 
-path_res <- "/dados/projetos_andamento/TRADEhub/trade_hub_plangea/land_uses"
+scen_subset <- scen_subset[-1]
 
-dir.create(path_res)
+for(i in 1:length(scen_subset)){
 
-# scenario subset - beggining of the loop
+  # folder to save the results
+  
+  path_res <-file.path( "/dados/projetos_andamento/TRADEhub/trade_hub_plangea/land_uses",scen_to_keep[i])
+  
+  # dir.create(path_res)-- ja criei no script exporting nat_areas.r
+  
+  urban_sub <- grep(urban,pattern = scen_subset[i],value = T) 
+  
+  # opening rasters
+  
+  urban_sub_r <- lapply(urban_sub,stack)
+  
+  urban_final_pj <- projectRaster(urban_sub_r[[1]],
+        crs = "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs ")
+  
+  # plot(urban_final)
+  
+  # folder for urban
+  
+  dir.create(file.path(path_res,"urban"))
+  
+  writeRaster(x = urban_final_pj,filename =file.path(path_res,"urban",
+              paste0(scen_subset[i],"_urban.tif")),overwrite=TRUE)
 
-i=1
-
-urban_sub <- grep(urban,pattern = scen_subset[i],value = T) # here I can replace with an index
-
-# opening rasters
-
-urban_sub_r <- lapply(urban_sub,stack)
-
-urban_final <- Reduce("+",urban_sub_r)
-
-urban_final_pj <- projectRaster(urban_final,
-      crs = "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs ")
-
-# plot(urban_final)
-
-# folder for agriculture
-
-dir.create(file.path(path_res,"urban"))
-
-writeRaster(x = urban_final_pj,filename =file.path(path_res,"urban",
-            paste0(scen[i],"_urban.tif")),overwrite=TRUE)
+}
