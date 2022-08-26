f96e6c9cc9ecc495caea3c009b5511c6438cb316
franciscodalbertas <francisco.albertas@gmail.com>
2022-08-26 16:39:12 -0300

    adicionando loop

--- scripts/merg_agri_IIASA_exporting_pasture.R
+++ scripts/merg_agri_IIASA_exporting_pasture.R
@@ -35,70 +35,68 @@ scen_to_keep <- c("TH_TFBASE_TCBASE_NOBIOD_NOTECH_NODEM_SPA0_SSP2",
 
 scen_subset <- grep(pattern =paste(scen_to_keep,collapse = "|"),x = scen,value = T )
 
+# como ja terminei o cen "TH_TF2000_TCBASE_BIOD_NOTECH_NODEM_SPA0_SSP2" exclui ele do loop
 
-# folder to save the results
+scen_to_keep <- scen_to_keep[-4]
 
-path_res <- "/dados/projetos_andamento/TRADEhub/trade_hub_plangea/land_uses"
+scen_subset <- scen_subset[-1]
 
-dir.create(path_res)
+for(i in 1:length(scen_subset)){
 
-# folder for agriculture
+  # folder to save the results
+  
+  path_res <- file.path("/dados/projetos_andamento/TRADEhub/trade_hub_plangea/land_uses",scen_to_keep[i])
+  
+  # dir.create(path_res)
+  
+  # folder for agriculture
+  
+  dir.create(file.path(path_res,"agriculture"))
+  
+  # scenario subset - beggining of the loop
+  
+  agri_sub <- grep(agri,pattern = scen_subset[i],value = T) 
+  
+  # opening rasters
+  
+  agri_sub_r <- lapply(agri_sub,stack)
+  
+  # sum agricultural uses!
+  
+  agriculture <- Reduce("+",agri_sub_r)
+  
+  crs  <-  "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs "
+  
+  agriculture_pj <- projectRaster( agriculture,crs = "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs ")
+  
+    # saving with scenario name
+  
+  writeRaster(x = agriculture_pj,filename =file.path(path_res,"agriculture",paste0(scen_subset[1],"_agriculture.tif")),overwrite=TRUE)
+  
+  # pastureland
+  
+  
+  pasture <- list.files(path = p,pattern = "grass*",recursive = T,full.names = T)
+  
+  # inicio do loop
+  
+  pasture_sub <- grep(pasture,pattern = scen_subset[i],value = T) # here I can replace with an index
+  
+  # opening rasters
+  
+  pasture_sub_r <- lapply(pasture_sub,stack)
+  
+  # sum agricultural uses!
+  
+  pasture <- Reduce("+",pasture_sub_r)
+  
+  pasture_pj <- projectRaster( pasture,crs = crs)
+  
+  # folder for pastureland
+  
+  dir.create(file.path(path_res,"pasture"))
+  
+  writeRaster(x = pasture_pj,filename =file.path(path_res,"pasture",
+    paste0(scen_subset[i],"_pasture.tif")),overwrite=TRUE)
 
-dir.create(file.path(path_res,"agriculture"))
-
-# scenario subset - beggining of the loop
-
-i=1
-
-agri_sub <- grep(agri,pattern = scen_subset[i],value = T) # here I can replace with an index
-
-# opening rasters
-
-agri_sub_r <- lapply(agri_sub,stack)
-
-# sum agricultural uses!
-
-agriculture <- Reduce("+",agri_sub_r)
-
-crs  <-  "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs "
-
-agriculture_pj <- projectRaster( agriculture,
-        crs = "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs ")
-
-# plot(agriculture)
-
-# saving with scenario name
-
-writeRaster(x = agriculture_pj,filename =file.path(path_res,"agriculture",
-                      paste0(scen[1],"_agriculture.tif")),overwrite=TRUE)
-
-################################################################################
-# pastureland
-################################################################################
-
-pasture <- list.files(path = p,pattern = "grass*",recursive = T,full.names = T)
-
-# inicio do loop
-
-pasture_sub <- grep(pasture,pattern = scen_subset[i],value = T) # here I can replace with an index
-
-# opening rasters
-
-pasture_sub_r <- lapply(pasture_sub,stack)
-
-# sum agricultural uses!
-
-pasture <- Reduce("+",pasture_sub_r)
-
-
-pasture_pj <- projectRaster( pasture,crs = crs)
-
-
-# plot(pasture)
-
-# folder for pastureland
-
-dir.create(file.path(path_res,"pasture"))
-
-writeRaster(x = pasture_pj,filename =file.path(path_res,"pasture",
-  paste0(scen[i],"_pasture.tif")),overwrite=TRUE)
+}
