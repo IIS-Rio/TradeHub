ef5d3fb53b3001d0eb5e46c2959d4f8ee4f556ca
franciscodalbertas <francisco.albertas@gmail.com>
2022-08-26 15:11:44 -0300

    atualizacao

--- scripts/exporting_LU_2050.R
+++ scripts/exporting_LU_2050.R
@@ -1,5 +1,5 @@
 library(raster)
-library(stringr)
+
 
 # criando os rasters de lu pro plangea, pra 2050
 
@@ -7,46 +7,71 @@ library(stringr)
 
 p <- "/dados/projetos_andamento/TRADEhub/trade_hub_plangea/land_uses"
 
-# usos que tiveram que ser "juntados"
 
-lu <- paste0(c("desert","grassland","rock_ice","shrubland","wetland","other_natland"),"_final")
+# listando pastas com usos finais (descarta usos agregados que foram desagregados; ex: restored e other)
 
-# usos restantes
+l_dir <- list.files(p)[c(1:5,7,9,11:14)]
 
-lu_2 <- c("agriculture","forest","pasture","urban")
-
-lu_final <-c(lu,lu_2)
-
-# listando pastas com caminho completo
-
-l_r <- list.files(file.path(p,lu_final),recursive = T,full.names = T)
+l_r <- list.files(file.path(p,l_dir),recursive = T,full.names = T)
 
 # listando raster so com nomes
 
-l_r_names <- list.files(file.path(p,lu_final),recursive = T)
+l_r_names <- tools::file_path_sans_ext(list.files(file.path(p,l_dir),recursive = T))
 
-# listando so land uses
-
-l_r_lu <- list.files(file.path(p),pattern = paste(lu_final,collapse = "|"))
 
 # exportando ano 2050
-i=1
 
 p2 <- "/dados/projetos_andamento/TRADEhub/trade_hub_plangea"
 
 dir.create(file.path(p2,"land_uses_2050"))
 
 
+# criando pasta pra cada cenario
+
+dir.create(file.path(p2,"land_uses_2050","TH_TF2000_TCBASE_BIOD_NOTECH_NODEM_SPA0_SSP2"))
+
+
+p3 <- file.path(p2,"land_uses_2050","TH_TF2000_TCBASE_BIOD_NOTECH_NODEM_SPA0_SSP2")
+
+i=1
 
 for( i in 1:length(l_r)){
   # abrindo raster
   r <- raster(l_r[i],band=5)
   # criando diretorio
-  dir.create(file.path(p2,"land_uses_2050",l_r_lu[i]))
-  save_path <- file.path(p2,"land_uses_2050",l_r_lu[i],l_r_names[i])
+  #dir.create(file.path(p2,"land_uses_2050",l_r_lu[i]))
+  save_path <- file.path(p3,paste0(basename(l_r_names[i]),"_2050.tif"))
   writeRaster(r,filename = save_path)
   
 }
+
+
+# testando soma 1
+
+soma <- lapply(list.files(p3,full.names = T),raster)
+soma_reduced <- Reduce("+",soma)
+
+plot(soma_reduced)
+
+summary(soma_reduced[])
+
+# salvando ultimo layer com fracoes que serao ignoradas
+
+ignored <- 1- soma_reduced
+
+
+# removendo valor negativo
+
+
+ignored[ignored<0] <- 0
+
+
+writeRaster(ignored,filename = file.path(p3,"TradeHubTrack1Prelim-LCproj-GLOBIOM-TH_TF2000_TCBASE_BIOD_NOTECH_NODEM_SPA0_SSP2-24May2022_FinalMask_55x55km_ignored_2050.tif"))
+
+
+# rename_p <- "/dados/projetos_andamento/TRADEhub/trade_hub_plangea/land_uses_backup/other_natland_disaggregated"
+# 
+# file.rename(from = list.files(rename_p,full.names = T),paste0(tools::file_path_sans_ext(list.files(rename_p,full.names = T)),"_other.tif"))
 
 
 
