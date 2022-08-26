b8bfb3565edeeda933ed10f60f40f8b1a296d802
franciscodalbertas <francisco.albertas@gmail.com>
2022-08-26 17:33:16 -0300

    adicionando loop

--- scripts/removing_ESA_from_other_natural.R
+++ scripts/removing_ESA_from_other_natural.R
@@ -2,7 +2,41 @@
 
 library(raster)
 
-other_nat_ssp2 = stack(list.files("/dados/projetos_andamento/TRADEhub/trade_hub_plangea/land_uses/other_natural_lands",full.names = T))
+
+# defining path
+
+p <- "/dados/projetos_andamento/TRADEhub/GLOBIOM/atualizacao/scen_desagregados"
+
+
+# list of scenarios (20 in total)
+
+scen <- gsub("_abn_cropland_2Gbioen_10.tif","",
+             list.files(file.path(p,"abn_cropland_2Gbioen_10"),pattern = "55"))
+
+
+# selecting 5 priority scenarios
+
+## baseline_TRADE + baseline_BTC : TH_TFBASE_TCBASE_NOBIOD_NOTECH_NODEM_SPA0_SSP2
+## exacerbated trade liberalization + IAP_BTC: TH_TFELIM_TCREDU_BIOD_TECH_DEM_SPA0_SSP2
+## frictions and reconfigurations + baseline_BTC: TH_TF2000_TCBASE_NOBIOD_NOTECH_NODEM_SPA0_SSP2
+## frictions and reconfigurations + C_BTC: TH_TF2000_TCBASE_BIOD_NOTECH_NODEM_SPA0_SSP2
+## baseline_TRADE + IAP_BTC: TH_TFBASE_TCBASE_BIOD_TECH_DEM_SPA0_SSP2
+
+scen_to_keep <- c("TH_TFBASE_TCBASE_NOBIOD_NOTECH_NODEM_SPA0_SSP2",
+                  "TH_TFELIM_TCREDU_BIOD_TECH_DEM_SPA0_SSP2",
+                  "TH_TF2000_TCBASE_NOBIOD_NOTECH_NODEM_SPA0_SSP2",
+                  "TH_TF2000_TCBASE_BIOD_NOTECH_NODEM_SPA0_SSP2",
+                  "TH_TFBASE_TCBASE_BIOD_TECH_DEM_SPA0_SSP2")
+
+# 5 scenarios
+
+scen_subset <- grep(pattern =paste(scen_to_keep,collapse = "|"),x = scen,value = T )
+
+# excluir o cenario piloto
+
+scen_to_keep <- scen_to_keep[-4]
+
+scen_subset <- scen_subset[-1]
 
 
 # ESA 1992
@@ -15,65 +49,62 @@ esa_raster_file_paths_nat <- grep(esa_raster_file_paths,pattern = paste(c("deser
 esa_rasters = lapply(esa_raster_file_paths_nat, raster)
 
 # Teste soma um
+
 esa_sum = Reduce('+', esa_rasters)
 
 # Reamostrando ESA natural
 
+### OBS !!!!!!!!! ##############################################
+
+# other_nat_ssp2 eh gerado no loop, tem q substituir essa parte!
+
+################################################################
 esa_nat_res = lapply(esa_rasters, function(r){raster::resample(x = r, y = other_nat_ssp2)})
 
 # Removendo negativos
 
 for (r in 1:length(esa_nat_res)) {
+  
   esa_nat_res[[r]][esa_nat_res[[r]] < 0] = 0
-}
 
-# Multiplicação do other por cada classe natural
+  }
 
-other_nat_ssp2_mult = lapply(esa_nat_res, function(r){r * other_nat_ssp2})
 
-# # Removendo negativos
-# for (r in 1:length(other_nat_ssp2_mult)) {
-#   other_nat_ssp2_mult[[r]][other_nat_ssp2_mult[[r]] < 0] = 0
-# }
-
-# Ler rasters de land-use do TradeHub (resolvi nao fazer essa parte agora)
-
-# lu_trade = list.files("/dados/projetos_andamento/TRADEhub/trade_hub_plangea/land_uses/", pattern = ".tif$", full.names = T, recursive = T)
-# lu_trade = lu_trade[-c(5, 7:13)]
-# lu_trade_ras = lapply(lu_trade, stack)
-# 
-# # Testando soma um após multiplicação
-# other_2050_mult_sum = Reduce('+', other_nat_ssp2_mult)
-# lu_trade_ras_sum = Reduce('+', lu_trade_ras)
-# 
-# test_sum_1 = other_2050_mult_sum + lu_trade_ras_sum
-# 
-# other_nat_new = 1 - test_sum_1
-# 
 files = gsub("(ESA_landuse_300m_1992_)(.*)(_media.*$)", "\\2", unlist(lapply(esa_nat_res, names)))
- 
+
 files[6] =  "grassland"
 
 
-other_2050_mult_sum = Reduce('+', other_nat_ssp2_mult)
+for (i in 1:length(scen_subset)){
 
-other_non_disagg <- other_nat_ssp2 -  other_2050_mult_sum
+  other_nat_ssp2 = stack(list.files(file.path("/dados/projetos_andamento/TRADEhub/trade_hub_plangea/land_uses",scen_to_keep[i],"other"),full.names = T))
+  
+  
+  # Multiplicação do other por cada classe natural
+  
+  other_nat_ssp2_mult = lapply(esa_nat_res, function(r){r * other_nat_ssp2})
+  
+  other_2050_mult_sum = Reduce('+', other_nat_ssp2_mult)
+  
+  other_non_disagg <- other_nat_ssp2 -  other_2050_mult_sum
+  
+  # Removendo negativos
+  for (r in 1:5) {
+    other_non_disagg[[r]][other_non_disagg[[r]] < 0] = 0
+  }
+  
+  
+  dir = file.path("/dados/projetos_andamento/TRADEhub/trade_hub_plangea/land_uses",scen_to_keep[i],"other_natland_disagg")
+  
+  dir.create(dir)
+  
+  scen_subset
+  
+  for(r in 1:length(other_nat_ssp2_mult)){
+    save.path <- file.path(dir, paste0(scen_subset[i],"_",files[r], "_other.tif"))
+    writeRaster(other_nat_ssp2_mult[[r]], save.path, overwrite = T)
+  }
+  
+  writeRaster(other_non_disagg, paste0(dir,"/" ,scen_subset[i], "_other_natland.tif" ))
 
-# Removendo negativos
-for (r in 1:5) {
-  other_non_disagg[[r]][other_non_disagg[[r]] < 0] = 0
 }
-
-
-dir = "/dados/projetos_andamento/TRADEhub/trade_hub_plangea/land_uses/other_natland_disagg//"
-
-dir.create(dir)
-
-
-
-for(r in 1:length(other_nat_ssp2_mult)){
-  save.path <- paste0(dir, "TradeHubTrack1Prelim-LCproj-GLOBIOM-TH_TF2000_TCBASE_BIOD_NOTECH_NODEM_SPA0_SSP2-24May2022_FinalMask_55x55km_", files[r], "_other.tif")
-  writeRaster(other_nat_ssp2_mult[[r]], save.path, overwrite = T)
-}
-
-writeRaster(other_non_disagg, paste0(dir, "TradeHubTrack1Prelim-LCproj-GLOBIOM-TH_TF2000_TCBASE_BIOD_NOTECH_NODEM_SPA0_SSP2-24May2022_FinalMask_55x55km_", "other_natland.tif" ))
