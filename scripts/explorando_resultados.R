b783b4e7c56c30e11d667d1757f3abdb9b630372
franciscodalbertas <francisco.albertas@gmail.com>
2022-09-26 17:01:20 -0300

    gerando paineis

--- scripts/explorando_resultados.R
+++ scripts/explorando_resultados.R
@@ -43,12 +43,27 @@ library(ggplot2)
 library(ggpubr)
 library(ggallin) # pseudolog trans
 library(viridis)
+library(RColorBrewer)
+library(scales)
 
 val_l <- df[,-c(2:6,26:29)] %>%
   pivot_longer(cols = grep(pattern = ".val",names(df[1,-c(2:6,26:29)])))
 
 val_l <- val_l[,c(1,16:17)]
 
+
+# adicionando nome da variavel
+
+# bd = débito de extinção de espécies
+# cb = carbon
+# ec = vulnerabilidade de ecorregiões
+# it = integridade de ecossistemas
+# oc = custo de oportunidade
+
+met_name <- data.frame(name = unique(val_l$name),metric=c("Carbon","Ecossistem's integrity","Ecoregion's vulnerability","Extinction debt","Land opportunity cost"))
+
+
+val_l <- left_join(val_l,met_name)
 
 # val_l$legend <- c("carbon","ecosystem integrity","ecoregion vulnerability","extinction risk","opportunity cost")
 
@@ -57,22 +72,11 @@ val_l <- val_l[,c(1,16:17)]
 
 
 scientific_10 <- function(x) {
-  parse(text=gsub("1e", "10^", scales::scientific_format()(x)))
+  parse(text=gsub("e", "%*% 10^", scales::scientific_format()(x)))
 }
 
 
 l <- c("BAU","frictions and reconfigurations + conservation","frictions and reconfigurations + BTC baseline","baseline + IAP","exacerbated liberalization + IAP")
-
-# l <- c("BAU","frictions and reconfigurations + conservation","baseline + IAP","exacerbated liberalization + IAP")
-
-plot <- ggplot(val_l, aes(x=name, y=value,fill=scenario_name)) + 
-  geom_bar(stat = "identity",position = position_dodge())+
-  scale_y_continuous(trans = pseudolog10_trans,breaks = c(10^2,10^4,10^6,10^8,10^10,0,-10^2,-10^4,-10^6,-10^8,-10^10,-10^12),label=scientific_10 ) +
-  theme_classic()+
-  xlab("plangea's metrics")+
-  ylab("Variation relative to present")+
-  scale_fill_viridis_d(name="scenario",labels=l)
-
 
 
 # bd = débito de extinção de espécies
@@ -81,32 +85,12 @@ plot <- ggplot(val_l, aes(x=name, y=value,fill=scenario_name)) +
 # it = integridade de ecossistemas
 # oc = custo de oportunidade
 
+#-------------------------------------------------------------------------------
+
 # Painel por variavel
 
+#-------------------------------------------------------------------------------
 
-ggplot(val_l, aes(x=name, y=value,fill=scenario_name)) + 
-  geom_bar(stat = "identity",position = position_dodge())+
-  scale_y_continuous(trans = pseudolog10_trans,breaks = c(10^2,10^4,10^6,10^8,10^10,0,-10^2,-10^4,-10^6,-10^8,-10^10,-10^12),label=scientific_10 ) +
-  theme_classic()+
-  xlab("plangea's metrics")+
-  ylab("Variation relative to present")+
-  scale_fill_viridis_d(name="scenario")+
-  coord_flip()+
-  facet_grid("scenario_name")
-
-ggplot(val_l, aes(x=scenario_name, y=value,fill=scenario_name)) + 
-  geom_bar(stat = "identity",position = position_dodge())+
-  scale_y_continuous(trans = pseudolog10_trans,breaks = c(10^2,10^4,10^6,10^8,10^10,0,-10^2,-10^4,-10^6,-10^8,-10^10,-10^12),label=scientific_10 ) +
-  theme_classic()+
-  xlab("plangea's metrics")+
-  ylab("Variation relative to present")+
-  scale_fill_viridis_d(name="scenario",labels=l)+
-  scale_x_discrete(labels= l)+
-  coord_flip()+
-  facet_grid("name")+
-  theme(legend.position = 'bottom', legend.direction = "horizontal")+
-  guides(fill=guide_legend(nrow=2,byrow=TRUE))
- 
 
 # rodar c+_ss
 # 1.todas de comercio com baseline de conservacao.
@@ -115,3 +99,132 @@ ggplot(val_l, aes(x=scenario_name, y=value,fill=scenario_name)) +
 # usar valores relativos ao bau 2050
 
 # pensar em rodar plangea  2020 BAU 
+
+metricas <- unique(val_l$metric)
+
+
+
+l <- c("BAU","frict.&reconfig. + conservation","frict.&reconfig. + BTC baseline","baseline + IAP","transp.cost. red + BTC baseline","tarif.elim.+BTC baseline","exacerb. lib. + IAP","exacerb. lib. + BTC baseline")
+
+nomes_scen <- data_frame(label_scen =l,scenario_name=unique(val_l$scenario_name))
+
+val_l <- left_join(val_l,nomes_scen)
+
+###############################################################################
+# versao com valores absolutos
+###############################################################################
+
+lista_todos_plots <- list()
+c=1
+for(metrica in metricas){
+  
+  gfico <- val_l %>%
+    filter(metric== metrica)%>%
+  ggplot( aes(x=scenario_name, y=value,fill=scenario_name)) + 
+    geom_bar(stat = "identity",position = position_dodge())+
+    scale_y_continuous(label=scientific_10 ) +
+    theme_classic()+
+    xlab(metrica)+
+    ylab("Absol. variation (baseline 2020)")+
+    scale_fill_brewer(palette = "Spectral",name="name",labels=l)+
+    geom_hline(yintercept=0, linetype="dashed",color = "red", size=1)+
+    coord_flip()+
+    theme(legend.position = 'bottom', legend.direction = "horizontal",
+          axis.text.y=element_blank(),
+          axis.ticks.y=element_blank())+
+    guides(fill=guide_legend(nrow=2,byrow=TRUE,title = ""))
+  lista_todos_plots[[c]] <- gfico
+  c = c+1
+}
+
+todos_plots <- ggarrange(plotlist = lista_todos_plots,common.legend = T)
+
+ggsave(filename = "figures/exploratory_8scen.jpeg",width = 25.4,height = 14.288,units = "cm",plot = todos_plots,bg ="white")
+
+# apenas cenarios comercio
+
+BTC <- c("BIOD_NOTECH_NODEM_SPA0_SSP2",
+         "BIOD_TECH_NODEM_SPA0_SSP2",
+         "BIOD_TECH_DEM_SPA0_SSP2")
+
+# selecionando BTC scen
+conserv <- grep(pattern = paste(BTC,collapse = "|"),x = val_l$scenario_name,value = T)
+
+
+# eliminando o baseline
+conserv_sub <- grep(pattern = "NOBIOD_NOTECH_NODEM_SPA0_SSP2",conserv,value = T,invert = T)
+
+
+
+lista_comercio <- list()
+
+c=1
+
+for(metrica in metricas){
+  
+  gfico <- val_l %>%
+    filter(metric== metrica)%>%
+    # tirando cenarios de conservacao
+    filter(!scenario_name %in% conserv_sub)%>%
+    ggplot( aes(x=label_scen, y=value,fill=label_scen)) + 
+    geom_bar(stat = "identity",position = position_dodge())+
+    scale_y_continuous(label=scientific_10 ) +
+    theme_classic()+
+    xlab(metrica)+
+    ylab("Absol. variation (baseline 2020)")+
+    scale_fill_brewer(palette = "Spectral",name="name")+
+    geom_hline(yintercept=0, linetype="dashed",color = "red", size=1)+
+    coord_flip()+
+    theme(legend.position = 'bottom', legend.direction = "horizontal",
+          axis.text.y=element_blank(),
+          axis.ticks.y=element_blank())+
+    guides(fill=guide_legend(nrow=2,byrow=TRUE,title = ""))
+    lista_comercio[[c]] <- gfico
+  c = c+1
+}
+
+comercio_plots <- ggarrange(plotlist = lista_comercio,common.legend = T)
+
+ggsave(filename = "figures/exploratory_Trade_scen.jpeg",width = 25.4,height = 14.288,units = "cm",plot = comercio_plots,bg ="white")
+
+###############################################################################
+# versao com valores relativos ao BAu 2050
+###############################################################################
+
+
+val_l <- val_l %>%
+  filter(metric== unique(metric)) %>%
+  mutate(relative_to_BAU_2050=value/value[label_scen=="BAU"])
+
+lista_todos_relative <- list()
+
+c=1
+
+for(metrica in metricas){
+  
+  gfico <- val_l %>%
+    filter(metric== metrica)%>%
+    # tirando cenarios de conservacao
+    filter(!scenario_name %in% conserv_sub)%>%
+    # tirando BAU
+    filter(label_scen != "BAU")%>%
+    ggplot( aes(x=label_scen, y=relative_to_BAU_2050,fill=label_scen)) + 
+    geom_bar(stat = "identity",position = position_dodge())+
+    #scale_y_continuous(label=scientific_10 ) +
+    theme_classic()+
+    xlab(metrica)+
+    ylab("Relat. variation (BAU 2050)")+
+    scale_fill_brewer(palette = "Spectral",name="name")+
+    geom_hline(yintercept=1, linetype="dashed",color = "darkgray", size=1)+
+    coord_flip()+
+    theme(legend.position = 'bottom', legend.direction = "horizontal",
+          axis.text.y=element_blank(),
+          axis.ticks.y=element_blank())+
+    guides(fill=guide_legend(nrow=2,byrow=TRUE,title = ""))
+  lista_todos_relative[[c]] <- gfico
+  c = c+1
+}
+
+todos_plots_relative <- ggarrange(plotlist = lista_todos_relative,common.legend = T)
+
+ggsave(filename = "figures/exploratory_Trade_scen.jpeg",width = 25.4,height = 14.288,units = "cm",plot = comercio_plots,bg ="white")
