c88b302fdd679dbcd0345d4dfa0c5bceb54c3f58
franciscodalbertas <francisco.albertas@gmail.com>
2024-02-21 19:34:50 -0300

    atualizando carbono

--- scripts/scripts_bd/02_run_reprojecting_spp_dist.r
+++ scripts/scripts_bd/02_run_reprojecting_spp_dist.r
@@ -1,7 +1,110 @@
-# SSP3 ou current
 
-current_or_SSP3 <- "SSP3"  # Set your conditional variable here
+#- pacotes ---------------------------------------------------------------------
+
 library(terra)
-# script que faz o reproject
-source("/dados/pessoal/francisco/TradeHub/scripts/scripts_bd/01_reprojecting_spp_dist.R")
+library(foreach)
+library(doParallel)
+library(snow)
+#-------------------------------------------------------------------------------
+# lista de gcms
+gcm_list = list.dirs(path = "/dados/bd_iis/climate_envelopes_fromCI/ppm_gcm", full.names = FALSE, recursive = FALSE) 
 
+files_to_future <- paste0("/dados/projetos_andamento/CBD-draft/rawdata/climate_envelopes/bien_intersections/")
+
+file_to_present <- "/dados/projetos_andamento/CBD-draft/rawdata/climate_envelopes/bien_intersections/bien_current/"
+
+
+output_path <- file.path("/dados/projetos_andamento/TRADEhub/trade_hub_plangea/rawdata/species_climate/SSP3")
+
+
+groups <- list.dirs(file_to_present,recursive = F,full.names = F)
+
+r <- rast("/dados/projetos_andamento/TRADEhub/trade_hub_plangea/rawdata/land-use-regional_2050/CAN/agriculture.tif")
+
+
+# listando raster names per group
+
+lst_rst_group <- list()
+for(g in seq_along(groups)){
+  lst_r <- basename(list.files(file.path(files_to_future,gcm_list[1],groups[g]), pattern = ".tif", full.names = TRUE))
+  nm <- groups[g]
+  df <- expand.grid(group=nm,raster=lst_r)
+  lst_rst_group[[g]] <- df
+  
+}
+
+
+files <- do.call(rbind,lst_rst_group)
+
+tasks <- merge(files,gcm_list)
+
+names(tasks)[3] <- "gcms"
+
+tasks <- tasks[1:10,]
+
+
+process_raster <- function(group,gcm,raster,input_path,output_path,current_or_SSP3) {
+  
+  # Load the raster package within the worker
+  library(terra)
+  
+  input_file <-(file.path(input_path,gcm,group,raster))
+  
+  # Read the input raster
+  
+  r2 <- rast(input_file)
+  
+  # Reproject the raster (replace the projection parameters with your target projection)
+  #target_projection <- crs
+  
+  r2 <- terra::project(r2, r,"near")
+  
+  # Define the output file name
+  output_file <- file.path(output_path,gcm,group,raster )
+  
+  
+  
+  # Save the reprojected raster
+  writeRaster(r2, filename = output_file, gdal=c("COMPRESS=DEFLATE"),overwrite=T)
+  
+}
+
+# Setting up the progress bar
+iterations = nrow(tasks)
+# Progress bar object
+pb_l = progress::progress_bar$new(
+  format = "Loading scenario [:bar] :percent in :elapsed",
+  total = iterations, clear = FALSE, width = 70)
+
+progress_number = 1:iterations
+progress = function(n) {pb_l$tick(tokens = list(sp = progress_number[n]))}
+opts = list(progress = progress)
+
+# Set the number of cores to use
+num_cores <- 25
+cl <- makeCluster(num_cores, outfile="")
+# start parallel processing
+doSNOW::registerDoSNOW(cl)
+
+# Run tasks in parallel
+foreach(i = 1:nrow(tasks),.packages = c('terra'),
+        .options.snow = opts,
+        .errorhandling = "remove") %dopar% {
+          
+          suppressWarnings(suppressMessages(devtools::load_all("/dados/pessoal/francisco/plangea-pkg/", quiet = TRUE)))
+          process_raster(group = tasks$group[i],gcm = tasks$gcms[i],raster = tasks$raster[i],input_path = files_to_future,output_path = output_path,current_or_SSP3 = "SSP3" )
+          
+        }
+
+# Stop the parallel cluster
+stopCluster(cl)
+
+
+#Error in unserialize(socklist[[n]]) : erro ao ler a partir de conexão
+
+
+for(i in 1:nrow(tasks)){
+  
+  process_raster(group = tasks$group[i],gcm = tasks$gcms[i],raster = tasks$raster[i],input_path = files_to_future,output_path = output_path)
+  
+}
--- scripts/scripts_figures/cb_barplots.R
+++ scripts/scripts_figures/cb_barplots.R
@@ -1,21 +1,46 @@
+library(data.table)
+library(tidyverse)
+library(scales)
+
 # combining new carbon data (move to another script when possible)
 
-df <- fread("/dados/projetos_em_andamento/  ")
+emited <- fread("output_tables/updated_results/carbon_emmited_carbonlayerIIS.csv")
+
+# convertendo em ha
+
+emited$cb_emission <- emited$cb_emission/100
+
+seq <- fread("output_tables/updated_results/carbon_sequetered_restoration_Naturemap.csv")
+
+
+
+
+df <- left_join(emited,seq)[,c(2:4,6,7,9)]
+
+names(df)[c(2,6)] <- c("region","cb_seq")
+
+df$net <- df$cb_emission+df$cb_seq
+
+
+df_l <- pivot_longer(df,c(3,6,7))
 
 #-------------------------------------------------------------------------------
 
 # testando stacked barplot com oc por regiao
+# valor carbono emitido ta superestimado
 
-total_cb_region$label_scen <- factor(total_cb_region$label_scen,levels = c("ETL","Ta","Tr","Fr","BAU","Trade-base"))
 
-p_BTC_base <- total_cb_region %>%
-  filter(conservation=="BTC-base")%>%
+df_l$label_scen <- factor(df_l$label_scen,levels = c("ETL","Ta","Tr","Fr","BAU","Trade-base"))
+
+p_BTC_base <- df_l %>%
+  filter(conservation=="BTC-base",
+         name=="net")%>%
   # calculate percentages
   group_by(conservation,label_scen)%>%
   #filter(label_scen==label_scen,region==region,conservation==conservation)%>%
-  mutate(prop=total_cb/sum(abs(total_cb)))%>%
+  mutate(prop=value/sum(abs(value)))%>%
   mutate(label=paste0(round(abs(prop),2)*100,"%"))%>%
-  mutate(total_cb_1000=total_cb/10^9)%>%
+  mutate(total_cb_1000=value/10^9)%>%
   # colocando em branco % < 1
   mutate(label=ifelse(abs(prop)>0.02,label,""))%>%
   arrange(prop) %>%
@@ -28,25 +53,26 @@ p_BTC_base <- total_cb_region %>%
   scale_fill_brewer(palette = "Set3", name="")+
   scale_y_continuous(labels = comma)+
   # incluindo net value
-  geom_point(data = total_cb[total_cb$conservation=="BTC-base",],aes(y=total_cb_1000,x=label_scen),fill="black",colour="black",shape=8, position=position_nudge(x = -0.2))+
+#  geom_point(data = total_cb[total_cb$conservation=="BTC-base",],aes(y=total_cb_1000,x=label_scen),fill="black",colour="black",shape=8, position=position_nudge(x = -0.2))+
   #facet_grid(~conservation)+
   theme_bw()+
   ggtitle("BTC-base")+
   xlab("")+
   ylab(expression(paste(CO[2], " balance ( billion tons) ", sep="")))+
   #ylab("CO2 balance ( billion tons)")+
-  ylim(-25,12)+
+  #ylim(-25,12)+
   guides(fill = guide_legend(reverse=TRUE))
 
 
-p_BTC_C <- total_cb_region %>%
-  filter(conservation!="BTC-base")%>%
+p_BTC_C <- df_l %>%
+  filter(conservation!="BTC-base",
+         name=="net")%>%
   # calculate percentages
   group_by(conservation,label_scen)%>%
   #filter(label_scen==label_scen,region==region,conservation==conservation)%>%
-  mutate(prop=total_cb/sum(abs(total_cb)))%>%
+  mutate(prop=value/sum(abs(value)))%>%
   mutate(label=paste0(round(abs(prop),2)*100,"%"))%>%
-  mutate(total_oc_1000=total_cb/10^9)%>%
+  mutate(total_oc_1000=value/10^9)%>%
   arrange(prop) %>%
   # colocando em branco % < 1
   mutate(label=ifelse(abs(prop)>0.02,label,""))%>%
@@ -59,13 +85,13 @@ p_BTC_C <- total_cb_region %>%
   scale_fill_brewer(palette = "Set3", name="")+
   scale_y_continuous(labels = comma)+
   # incluindo net value
-  geom_point(data = total_cb[total_cb$conservation!="BTC-base",],aes(y=total_cb_1000,x=label_scen),fill="black",colour="black",shape=8, position=position_nudge(x = -0.3))+
+#  geom_point(data = total_cb[total_cb$conservation!="BTC-base",],aes(y=total_cb_1000,x=label_scen),fill="black",colour="black",shape=8, position=position_nudge(x = -0.3))+
   #facet_grid(~conservation)+
   theme_bw()+
   ggtitle("C")+
   xlab("")+
   ylab(expression(paste(CO[2], " balance ( billion tons) ", sep="")))+
-  ylim(-25,12)+
+  #ylim(-25,12)+
   guides(fill = guide_legend(reverse=TRUE))
 
 p_BTC_base <- ggpar(p = p_BTC_base,font.caption = c("plain",7,"black"),font.x = c("plain",7,"black"),font.y = c("plain",9,"black"),font.legend =c("plain",7,"black"),font.tickslab = c("plain",7,"black"),font.main = c("bold",9,"black") )
